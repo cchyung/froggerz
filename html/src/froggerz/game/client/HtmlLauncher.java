52fbe37ca2bdff97e04211a7b97e210ffc09ef6d
Conner Chyung <chyungspice@gmail.com>
2017-11-08 21:39:44 -0800

    Fix repo

--- html/src/froggerz/game/client/HtmlLauncher.java
+++ html/src/froggerz/game/client/HtmlLauncher.java
@@ -3,17 +3,17 @@ package froggerz.game.client;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import froggerz.game.Game;
+import froggerz.game.main;
 
 public class HtmlLauncher extends GwtApplication {
 
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(Game.WIDTH, Game.HEIGHT);
+                return new GwtApplicationConfiguration(480, 320);
         }
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new Game();
+                return new main();
         }
 }
