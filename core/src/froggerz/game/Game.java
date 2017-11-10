982ce572a02a31738c2f5c7e0e5ad21ba82d18e2
Ashley Thomas <avthomas@usc.edu>
2017-11-10 15:16:50 -0800

    File name change

--- core/src/froggerz/game/Game.java
+++ core/src/froggerz/game/Game.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
@@ -45,7 +46,7 @@ public class Game extends ApplicationAdapter {
 		
 		batch = new SpriteBatch();
 		manager = new AssetManager();
-		
+
 		loadData();
 	}
 
